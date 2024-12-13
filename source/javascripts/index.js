efdef9bb8b567c5cb5d8979adcf40e188c8f5366
codeaquil <cyndaquil0130@gmail.com>
2024-12-13 16:34:51 -0500

    fix links; set year with javascript

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,1 +1,12 @@
 import 'bootstrap';
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("DOM content loaded");
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+  let yearElement = document.getElementById('year');
+  if (yearElement) {
+    yearElement.textContent = new Date().getFullYear();
+  }
+});
