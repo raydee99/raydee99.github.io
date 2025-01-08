40f015b41d0a8e7ce91f3fbc321f92c8c016bd2e
codeaquil <cyndaquil0130@gmail.com>
2025-01-07 23:10:52 -0500

    disable animations after first visit per session

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -2,6 +2,20 @@ import 'bootstrap';
 
 document.addEventListener('DOMContentLoaded', function() {
   console.log("DOM content loaded");
+
+  if( window.sessionStorage.getItem('had_first_visit') ) {
+    const nav = document.querySelector('nav')
+    nav.classList.add('no-animations');
+    nav.classList.remove('drop-in');
+
+    const footer = document.querySelector('footer');
+    footer.classList.add('no-animations');
+    footer.classList.remove('fade-in');
+  }
+  else {
+    window.sessionStorage.setItem('had_first_visit', 'true');
+  }
+
 })
 
 document.addEventListener('DOMContentLoaded', function() {
